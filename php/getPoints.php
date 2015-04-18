798cc0ccac18ec31a66ae9f07bf549862f1b5171
Mathieu Alorent <github@kumy.net>
2015-04-18 03:11:18 +0200

    Import missing scripts

---
+++ php/getPoints.php
@@ -0,0 +1,74 @@
+<?php
+
+$specifier='all';
+
+require_once("config/config.inc.php");
+require_once("lib/BaseXClient.php");
+
+header('Content-Type: application/xml; charset=utf-8');
+echo '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+<gkxml version="1.0" date="'.date("Y-m-d H:i:s").'">';
+
+try {
+  // create session
+  $session = new Session($BASEX_SERVER, $BASEX_PORT, $BASEX_USERNAME, $BASEX_PASSWORD);
+
+  $file = "xquery/select-moves.xq";
+  if (isset($_GET['ghosts'])) {
+    $file = "xquery/select-moves-ghosts.xq";
+    $specifier='Ghosts';
+  } else if (isset($_GET['older'])) {
+    $file = "xquery/select-moves-older.xq";
+    $specifier='Older';
+  } else if (isset($_GET['newer'])) {
+    $file = "xquery/select-moves-newer.xq";
+    $specifier='Newer';
+  }
+
+  $query = $session->query(file_get_contents($file));
+  if (isset($_GET['limit'])) {
+    $query->bind('limit', $_GET['limit'], "xs:integer");
+  }
+
+  // parse gkid
+  if (isset($_GET['gkid'])) {
+    $query->bind('gkid', $_GET['gkid'], "xs:integer");
+    $result = $query->execute();
+    print $result;
+  // parse waypoints or lat/lon
+  } else if (isset($_GET['wpt']) && isset($_GET['lat']) && isset($_GET['lon'])) {
+    $query->bind('wpt', $_GET['wpt'], "xs:string");
+    $query->bind('lat', round($_GET['lat'], 5), "xs:float");
+    $query->bind('lon', round($_GET['lon'], 5), "xs:float");
+    $result = $query->execute();
+    print $result;
+  // parse waypoints
+  } else if (isset($_GET['wpt'])) {
+    $query->bind('wpt', $_GET['wpt'], "xs:string");
+    $result = $query->execute();
+    print $result;
+  // parse map space
+  } else if (isset($_GET['latTL']) and isset($_GET['lonTL']) and isset($_GET['latBR']) and isset($_GET['lonBR'])) {
+    $query->bind('latTL', $_GET['latTL'], "xs:float");
+    $query->bind('lonTL', $_GET['lonTL'], "xs:float");
+    $query->bind('latBR', $_GET['latBR'], "xs:float");
+    $query->bind('lonBR', $_GET['lonBR'], "xs:float");
+    print $query->execute();
+  // parse position
+  } else if (isset($_GET['lat']) and isset($_GET['lon'])) {
+    $query->bind('lat', round($_GET['lat'], 5), "xs:float");
+    $query->bind('lon', round($_GET['lon'], 5), "xs:float");
+    print $query->execute();
+  }
+
+  // close query instance
+  $query->close();
+
+  // close session
+  $session->close();
+
+echo '</gkxml>';
+} catch (Exception $e) {
+    echo '<error>',  $e->getMessage(), "</error>";
+}
+?>
---
+++ php/lookup.php
@@ -0,0 +1,22 @@
+<?php
+
+$gkid=0;
+
+if (isset($_GET['gk'])) {
+   $gk = $_GET['gk'];
+   $gkid = hexdec($gk);
+   echo '<h1>GK'.strtoupper($gk).'</h1>';
+} else if (isset($_GET['gkid'])) {
+   $gkid = $_GET['gkid'];
+}
+
+if ($gkid > 0) {
+   $link='http://geokrety.org/konkret.php?id='.$gkid;
+   echo '<h2>'.$gkid.'</h2>';
+   echo '<p><a href="'.$link.'">View on GeoKrety.org</a></p>';
+   header("Location: $link");
+} else {
+   echo "Sorry, no such id.";
+}
+
+?>
