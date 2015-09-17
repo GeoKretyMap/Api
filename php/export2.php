11d5321fab252928e6b3bdc63dcc9c1e7882ccce
Mathieu Alorent <github@kumy.net>
2015-09-18 00:14:38 +0200

    Export to GeoJson

--- php/export2.php
+++ php/export2.php
@@ -1,24 +1,25 @@
 <?php
 
-require_once("../../../../../../config/config.inc.php");
-require_once("lib/BaseXClient.php");
-
-header('Content-Type: application/xml; charset=utf-8');
-echo '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-<gkxml version="1.0" date="'.date("Y-m-d H:i:s").'">';
+require_once('../../../../../../config/config.inc.php');
+require_once('lib/BaseXClient.php');
 
 function getQuery($session, $file) {
-  $query = $session->query(file_get_contents("../xquery/$file"));
+  $query = $session->query(file_get_contents('../xquery/$file'));
 
   if (isset($_GET['limit'])) {
-    $query->bind('limit', $_GET['limit'], "xs:integer");
+    $query->bind('limit', $_GET['limit'], 'xs:integer');
   }
   return $query;
 }
 
-$detailed="";
+$detailed='';
 if (isset($_GET['details'])) {
-  $detailed="detailed-";
+  $detailed='detailed-';
+}
+
+$json='';
+if (isset($_GET['json'])) {
+  $json='-json';
 }
 
 try {
@@ -29,50 +30,64 @@ try {
   // parse gkid
   if (isset($_GET['gkid'])) {
     $query = getQuery($session, 'select-'.$detailed.'by-gkid.xq');
-    $query->bind('gkid', $_GET['gkid'], "xs:string");
+    $query->bind('gkid', $_GET['gkid'], 'xs:string');
   // parse waypoints or lat/lon
   } else if (isset($_GET['wpt']) && isset($_GET['lat']) && isset($_GET['lon'])) {
     $query = getQuery($session, 'select-'.$detailed.'by-wpt-or-coord-center.xq');
-    $query->bind('wpt', $_GET['wpt'], "xs:string");
-    $query->bind('lat', round($_GET['lat'], 5), "xs:float");
-    $query->bind('lon', round($_GET['lon'], 5), "xs:float");
+    $query->bind('wpt', $_GET['wpt'], 'xs:string');
+    $query->bind('lat', round($_GET['lat'], 5), 'xs:float');
+    $query->bind('lon', round($_GET['lon'], 5), 'xs:float');
   // parse waypoints
   } else if (isset($_GET['wpt'])) {
     $query = getQuery($session, 'select-'.$detailed.'by-wpt.xq');
-    $query->bind('wpt', $_GET['wpt'], "xs:string");
+    $query->bind('wpt', $_GET['wpt'], 'xs:string');
   // parse multiple waypoints
   } else if (isset($_GET['wpts'])) {
     $query = getQuery($session, 'select-by-wpts.xq');
-    $query->bind('wpts', $_GET['wpts'], "xs:string");
+    $query->bind('wpts', $_GET['wpts'], 'xs:string');
   // parse map space
   } else if (isset($_GET['latTL']) and isset($_GET['lonTL']) and isset($_GET['latBR']) and isset($_GET['lonBR'])) {
     $query = null;
     if (isset($_GET['ghosts'])) {
-      $query = getQuery($session, "select-by-coord-range-extended.xq");
-      $query->bind('ghosts', 1, "xs:integer");
+      $query = getQuery($session, 'select-by-coord-range-extended.xq');
+      $query->bind('ghosts', 1, 'xs:integer');
     } else if (isset($_GET['older'])) {
-      $query = getQuery($session, "select-by-coord-range-extended.xq");
-      $query->bind('older', 1, "xs:integer");
+      $query = getQuery($session, 'select-by-coord-range-extended.xq');
+      $query->bind('older', 1, 'xs:integer');
     } else if (isset($_GET['newer'])) {
-      $query = getQuery($session, "select-by-coord-range-extended.xq");
-      $query->bind('newer', 1, "xs:integer");
+      $query = getQuery($session, 'select-by-coord-range-extended.xq');
+      $query->bind('newer', 1, 'xs:integer');
     } else {
-      $query = getQuery($session, "select-'.$detailed.'by-coord-range.xq");
+      $query = getQuery($session, 'select-'.$detailed.'by-coord-range'.$json.'.xq');
     }
-    $query->bind('latTL', $_GET['latTL'], "xs:float");
-    $query->bind('lonTL', $_GET['lonTL'], "xs:float");
-    $query->bind('latBR', $_GET['latBR'], "xs:float");
-    $query->bind('lonBR', $_GET['lonBR'], "xs:float");
+    $query->bind('latTL', round($_GET['latTL'], 5), 'xs:float');
+    $query->bind('lonTL', round($_GET['lonTL'], 5), 'xs:float');
+    $query->bind('latBR', round($_GET['latBR'], 5), 'xs:float');
+    $query->bind('lonBR', round($_GET['lonBR'], 5), 'xs:float');
   // parse position
   } else if (isset($_GET['lat']) and isset($_GET['lon'])) {
     $query = getQuery($session, 'select-'.$detailed.'by-coord-center.xq');
-    $query->bind('lat', round($_GET['lat'], 5), "xs:float");
-    $query->bind('lon', round($_GET['lon'], 5), "xs:float");
+    $query->bind('lat', round($_GET['lat'], 5), 'xs:float');
+    $query->bind('lon', round($_GET['lon'], 5), 'xs:float');
   }
 
   if ($query !== null) {
     // return the datas
-    print $query->execute();
+    //print $query->execute();
+
+    $xml_string = '';
+    if ($json) {
+      header('Content-Type: application/json');
+      $xml_string .= $query->execute();
+    } else {
+      header('Content-Type: application/xml; charset=utf-8');
+      $xml_string .= '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>';
+      $xml_string .= '<gkxml version="1.0" date="'.date('Y-m-d H:i:s').'">';
+      $xml_string .= $query->execute();
+      $xml_string .= '</gkxml>';
+    }
+
+    echo $xml_string;
 
     // close query instance
     $query->close();
@@ -81,9 +96,7 @@ try {
   // close session
   $session->close();
 
-  echo '</gkxml>';
 } catch (Exception $e) {
-  echo '<error>',  $e->getMessage(), "</error>";
-  echo '</gkxml>';
+  echo $e->getMessage();
 }
 ?>
---
+++ xquery/select-by-coord-range-json.xq
@@ -0,0 +1,135 @@
+xquery version "1.0";
+
+declare namespace functx = "http://www.functx.com";
+
+
+declare function functx:add-months
+  ( $date as xs:anyAtomicType? ,
+    $months as xs:integer )  as xs:date? {
+
+   xs:date($date) + functx:yearMonthDuration(0,$months)
+ };
+declare function functx:yearMonthDuration
+  ( $years as xs:decimal? ,
+    $months as xs:integer? )  as xs:yearMonthDuration {
+
+    (xs:yearMonthDuration('P1M') * functx:if-empty($months,0)) +
+    (xs:yearMonthDuration('P1Y') * functx:if-empty($years,0))
+ };
+declare function functx:if-empty
+  ( $arg as item()? ,
+    $value as item()* )  as item()* {
+
+  if (string($arg) != '')
+  then data($arg)
+  else $value
+ };
+declare function functx:repeat-string
+  ( $stringToRepeat as xs:string? ,
+    $count as xs:integer )  as xs:string {
+
+   string-join((for $i in 1 to $count return $stringToRepeat),
+                        '')
+ };
+declare function functx:pad-integer-to-length
+  ( $integerToPad as xs:anyAtomicType? ,
+    $length as xs:integer )  as xs:string {
+
+   if ($length < string-length(string($integerToPad)))
+   then error(xs:QName('functx:Integer_Longer_Than_Length'))
+   else concat
+         (functx:repeat-string(
+            '0',$length - string-length(string($integerToPad))),
+          string($integerToPad))
+ };
+declare function functx:date
+  ( $year as xs:anyAtomicType ,
+    $month as xs:anyAtomicType ,
+    $day as xs:anyAtomicType )  as xs:date {
+
+   xs:date(
+     concat(
+       functx:pad-integer-to-length(xs:integer($year),4),'-',
+       functx:pad-integer-to-length(xs:integer($month),2),'-',
+       functx:pad-integer-to-length(xs:integer($day),2)))
+ };
+
+declare variable $latTL external;
+declare variable $lonTL external;
+declare variable $latBR external;
+declare variable $lonBR external;
+declare variable $limit external := 500;
+declare variable $ghosts external := 0;
+declare variable $older external := 0;
+declare variable $newer external := 0;
+
+
+let $today := current-date()
+let $year := year-from-date($today)
+let $month := month-from-date($today)
+let $day := day-from-date($today)
+let $date := functx:date($year, $month, $day)
+let $basedate := functx:add-months($date, -3)
+
+let $input   := doc("geokrety")/gkxml/geokrety/geokret
+let $filter1 := if ($older  > 0) then $input[@date <  $basedate]
+           else if ($newer  > 0) then $input[@date >= $basedate]
+           else if ($ghosts > 0) then $input[not(@state="0" or @state="3")]
+           else                       $input[   (@state="0" or @state="3")]
+
+let $result := if ($latTL castable as xs:float and $lonTL castable as xs:float
+               and $latBR castable as xs:float and $lonBR castable as xs:float)
+               then $filter1[xs:float(@lat) <= xs:float($latTL) and xs:float(@lon) <= xs:float($lonTL) and xs:float(@lat) >= xs:float($latBR) and xs:float(@lon) >= xs:float($lonBR)]
+               else "<error>'latTL/lonTL/latBR/lonBR' has an invalid type</error>"
+(:
+{
+   "type":"FeatureCollection",
+   "features":[
+      {
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               6.57052,
+               43.73088
+            ]
+         },
+         "type":"Feature",
+         "properties":{
+            "popupContent":"Trojan"
+         }
+      },
+   ]
+}
+<![CDATA[<br /><img src=\"http://geokretymap.org/gkimage/]]>"{string($a/@image)}<![CDATA[" />]]>
+:)
+
+return         
+json:serialize(
+<json type="object">
+  <type>FeatureCollection</type>
+  <features type="array">
+{for $a in subsequence($result, 1, $limit)
+  return
+    <_ type="object">
+      <geometry type="object">
+        <type>Point</type>
+        <coordinates type="array">
+          <_ type="number">{$a/@lon/number()}</_>
+          <_ type="number">{$a/@lat/number()}</_>
+        </coordinates>
+      </geometry>
+      <type>Feature</type>
+      <properties type="object">
+        <popupContent>{
+'<h1><a href="http://geokretymap.org/' || $a/@id || '" target="_blank">' || $a/data() || '</a></h1>' ||
+string(if ($a/@waypoint) then 'In <a href="http://geokrety.org/go2geo/index.php?wpt=' || $a/@waypoint || '" target="_blank">' || $a/@waypoint || '</a><br />' else '') ||
+string(if ($a/@date) then 'Last move: ' || $a/@date || '<br />' else '') ||
+'Travelled: ' || $a/@dist || ' km<br />' ||
+string(if ($a/@image) then '<img src="http://geokretymap.org/gkimage/' || $a/@image || '" width="100" />' else '')
+}</popupContent>
+      </properties>
+    </_>
+}
+  </features>
+</json>
+)
